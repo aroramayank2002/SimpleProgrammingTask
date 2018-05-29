43b6af6838d13afc769081124cbe9cb71252d50e
Mayank Arora <mayank.arora.ext@netent.com>
2018-05-29 11:31:46 +0200

    First approach stack trace error, updating to second approach

--- sum.js
+++ sum.js
@@ -2,11 +2,11 @@ let startNumber = 1;
 let endNumber = 5;
 
 function sumToNumber(startNumber, endNumber){
-    if (startNumber === endNumber)
-        return startNumber;
-    else
-    return startNumber + sumToNumber(startNumber+1, endNumber);
-
+  let sum = 0;
+  for(let i = startNumber; i <= endNumber ; i++){
+    sum += i;
+  }
+  return sum;
 }
 
 console.log(`Sum of numbers from ${startNumber} to ${endNumber} : ${sumToNumber(startNumber,endNumber)}`);
